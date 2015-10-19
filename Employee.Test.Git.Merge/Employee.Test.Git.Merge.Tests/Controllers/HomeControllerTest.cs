6c51de4f0995544a5ec2f7f5345dc365ec884d9e
Robert Sebescen <robert.sebescen@gmail.com>
2015-10-19 20:50:43 +0200

    added test

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge.Tests/Controllers/HomeControllerTest.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge.Tests/Controllers/HomeControllerTest.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Employee.Test.Git.Merge;
 using Employee.Test.Git.Merge.Controllers;
+using Employee.Test.Git.Merge.Repositories;
+using Employee.Test.Git.Merge.Services;
+using Moq;
 
 namespace Employee.Test.Git.Merge.Tests.Controllers
 {
@@ -49,6 +52,27 @@ namespace Employee.Test.Git.Merge.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestMerge()
+        {
+            var service = new TestService(new TestRepository());
+
+            var validData = new List<string>()
+            {
+                "VALID-4",
+                "VALID-5",
+                "VALID-6",
+                "VALID-7",
+                "VALID-8"
+            };
+
+            var actualData = service.GetData();
+            for (int i = 0; i < validData.Count; i++)
+            {
+                Assert.AreEqual(actualData[i], validData[i]);
+            }
         }
     }
 }
