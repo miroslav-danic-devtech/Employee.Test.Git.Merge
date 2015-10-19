dbcb6e0bc71fdc5cabe48df4d0fc612a5c2f9bd2
Robert Sebescen <robert.sebescen@gmail.com>
2015-10-19 21:10:34 +0200

    started using xunit

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge.Tests/Controllers/HomeControllerTest.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge.Tests/Controllers/HomeControllerTest.cs
@@ -3,58 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Employee.Test.Git.Merge;
 using Employee.Test.Git.Merge.Controllers;
 using Employee.Test.Git.Merge.Repositories;
 using Employee.Test.Git.Merge.Services;
 using Moq;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Employee.Test.Git.Merge.Tests.Controllers
 {
-    [TestClass]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
+        [Fact]
         public void TestMerge()
         {
             var service = new TestService(new TestRepository());
